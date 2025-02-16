aaa35b554488a82b4914c5a7d0269d42118cac62
Danny Sullivan <daniel-sullivan@pluralsight.com>
2025-02-16 18:19:07 -0500

    updates navbar

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,9 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["menu"]
+  static targets = ["menu", "mobileMenu"]
 
-  toggle() {
-    this.menuTarget.classList.toggle("hidden")
+  connect() {
+    // Close menu when clicking outside
+    document.addEventListener('click', this.closeMenuOutside.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener('click', this.closeMenuOutside.bind(this))
+  }
+
+  toggleMobile(event) {
+    event.stopPropagation()
+    this.mobileMenuTarget.classList.toggle("hidden")
+  }
+
+  closeMenuOutside(event) {
+    if (!this.element.contains(event.target)) {
+      this.mobileMenuTarget.classList.add("hidden")
+    }
   }
 }
