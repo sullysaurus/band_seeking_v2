4c6560da285983d075253016e19d20ffbdfef8f6
Danny Sullivan <daniel-sullivan@pluralsight.com>
2025-02-17 10:21:56 -0500

    fixes sign out button

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   
   devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
+    delete '/users/sign_out' => 'devise/sessions#destroy'     
   end
 
   # Consolidated Profiles routes under /musicians:
